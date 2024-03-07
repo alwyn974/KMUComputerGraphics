651103012068d10aaee3dbd5b804a5a05ec173d1
Alwyn974 <alwyn.mattapullut@epitech.eu>
2024-03-07 17:43:52 +0900

    wip: Trying cyclone

--- main.cpp
+++ main.cpp
@@ -11,8 +11,36 @@
 
 int main()
 {
+    // Move point p(1,4,-5) to vector v(5,1,4)
     cyclone::Vector3 p(1, 4, -5);
     cyclone::Vector3 v(5, 1, 4);
+    p += v;
 
-    std::cout << "pv " << (v + p).toString() << std::endl;
+    std::cout << "p + v: " << p.toString() << std::endl;
+
+    // Add vector v1(1,0,0) and vector v2(0,1,0)
+    cyclone::Vector3 v1(1, 0, 0);
+    cyclone::Vector3 v2(0, 1, 0);
+    cyclone::Vector3 v3 = v1 + v2;
+
+    std::cout << "v1 + v2: " << v3.toString() << std::endl;
+
+    // Multiply v1(0,1,0) with scalar 5
+    v1 = cyclone::Vector3(0, 1, 0);
+    v1 *= 5;
+
+    std::cout << "v1 * 5: " << v1.toString() << std::endl;
+
+    // Get a vector v starting at (8,2,5) and ending at (1,-1,4)
+    cyclone::Vector3 start(8, 2, 5);
+    cyclone::Vector3 end(1, -1, 4);
+    cyclone::Vector3 v4 = end - start;
+
+    std::cout << "v4: " << v4.toString() << std::endl;
+
+    // Normalize vector v(1,3,4)
+    cyclone::Vector3 v5(1, 3, 4);
+    v5.normalise();
+
+    std::cout << "v5: " << v5.toString() << std::endl;
 }
